ab94a114511aa60d36e43120cbdcf2d5b3ec2cac
Agoitz2 <93652574+Agoitz2@users.noreply.github.com>
2023-03-13 11:27:45 +0100

    Actualizo el color para que se adecue

---
+++ landing_page/componente/about/about.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import './about.css'
+import about from '../../images/pics/logo.png'
+// import text from '../../locales/'
+
+class About extends Component
+{
+	render()
+	{
+		return (
+			<section className = 'about py-5 my-5' id = 'about'>
+				<div className = 'container'>
+					<div className= "row align-items-center">
+						<div className = "c col-md-6 col-12">
+							<div className = 'left-side text-md-start text-center mb-lg-0 mb-5'>
+								<h3 className = 'about-name'><span>B</span>uen<span>V</span>iaje</h3>
+								<p className = 'about-description text-muted'>Lorem ipsum dolor sit amet</p>
+								{/* <p className = 'about-text'>{texto}</p> */}
+								<button className = 'btn btn-outline-danger text-capitalize btn-sm shadow-lg'>read more<i className="ms-2 fas fa-chevron-right"></i></button>
+							</div>
+						</div>
+						<div className = "c col-md-6 col-12">
+							<div className = 'right-side text-lg-end text-center'>
+								<img className = 'img-fluid w-75 shadow-lg' src = {about} alt = 'about' />
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		)
+	}
+}
+
+export default About
---
+++ landing_page/componente/navbar/navbar.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react'
+import './navbar.css'
+import {navbar_items} from '../helpers/Data'
+import {navbar_language} from '../helpers/Data'
+// import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
+
+
+class Navbar extends Component
+{
+	display_navbar_items = _ =>
+	{
+		let items = navbar_items.map(item =>
+		{
+			return (
+				<li className = "nav-item link" key = {Math.random()}>
+			        <a className = "nav-link p-md-0 py-md-1 px-md-2 me-md-2" href = {`#${item.link}`}><i className = {`${item.icon} me-2`}></i>{item.name}</a>
+			    </li>
+			)
+		})
+		return items
+	}
+
+	display_navbar_accounts = _ =>
+	{
+		let accounts = navbar_language.map(item =>
+		{
+			return (
+				// <li className="nav-item px-1 icon" key = {Math.random()}>
+	        	// 	<a className="nav-link" target = '_blank' rel="noreferrer">
+	        	// 		<Dropdown>
+				// 			<DropdownToggle>
+				// 				{/* <i className = {item.icon} onClick={}></i> */}
+				// 			</DropdownToggle>
+				// 		</Dropdown>
+				// 		<DropdownMenu>
+				// 			<DropdownItem>es</DropdownItem>
+				// 			<DropdownItem>eu</DropdownItem>
+				// 		</DropdownMenu>
+				<li className = "nav-item link" key = {Math.random()}>
+			        <a className = "nav-link p-md-0 py-md-1 px-md-2 me-md-2" href = {`#${item.link}`}>{item.name}</a>
+			    </li>
+						// <div class="navbar__languages">
+                              
+						// <ul class="lang-nav">
+						// 		<li class="selected">
+						// 	<a>EN</a>
+						// 	</li>
+						// 		<li>
+						// 	<a href="/nl/festival/welkom">NL</a>
+						// 	</li>
+						// 		<li>
+						// 	<a href="/fr/festival/bienvenue">FR</a>
+						// 	</li>
+						// 	</ul>
+						// </div>
+	        	// 	</a>
+	      		// </li>
+			)
+		})
+		return accounts
+	}
+
+	render()
+	{
+		return (
+			<nav className="navbar navbar-expand-md sticky-top active py-1 mb-5" id = 'navbar'>
+				<div className = 'container'>
+			 		<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			    		<span className="navbar-toggler-icon">Ξ</span>
+			  		</button>
+			  		<div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+			    		<ul className="navbar-nav mr-auto">
+			    			{this.display_navbar_items()}
+			    		</ul>
+			    		<ul className = "navbar-nav ml-auto">
+			    			{this.display_navbar_accounts()}
+			    		</ul>
+			    	</div>
+			    </div>
+			</nav>
+		)
+	}
+}
+
+export default Navbar
